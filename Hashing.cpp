89b204e9f333613d97ea4b062cbb2397d993dfd0
Ansh <anshuman302004@gmail.com>
2026-01-07 23:59:08 +0530

    learn: hashingg implementation using string

--- Hashing.cpp
+++ Hashing.cpp
@@ -1,29 +1,50 @@
 #include  <iostream>
 using namespace std;
 
+// ------------ Number Hashing ------------
+// int main() {
+//     int n;
+//     cout << "Enter the number of elements in the array: ";
+//     cin >> n;
+//     int arr[n];
+//     cout << "Enter the elements of the array: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//
+//     // precompute
+//     int hash[n + 1] = {0};
+//     for (int i = 0; i < n; i++) {
+//         hash[arr[i]]++;
+//     }
+//
+//     // cout << "Enter how many num"
+//     int h;
+//     cin >> h;
+//     while (h--) {
+//         int num;
+//         cin >> num; // Fetch
+//         cout << hash[num] << endl;
+//     }
+//     return 0;
+// }
+
+// ------------ Character Hashing ------------
 int main() {
-    int n;
-    cout << "Enter the number of elements in the array: ";
-    cin >> n;
-    int arr[n];
-    cout << "Enter the elements of the array: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    string s;
+    cout << "Enter a string: ";
+    cin >> s;
+    int hash[26] = {0};
+    for (int i = 0; i < s.length(); i++) {
+        hash[s[i] - 'a']++;
     }
 
-    // precompute
-    int hash[n + 1];
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]] += 1;
-    }
-
-    // cout << "Enter how many num"
     int h;
     cin >> h;
     while (h--) {
-        int num;
-        cin >> num; // Fetch
-        cout << hash[num] << endl;
+        char c;
+        cin >> c; // Fetch
+        cout << hash[c - 'a'] << endl;
     }
     return 0;
 }
